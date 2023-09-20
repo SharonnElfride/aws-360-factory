2c702acdcf40de0029da21a6427b14919b5c21a1
Sharonn <sharonn.zounon@clarilog.com>
2023-09-20 21:52:46 +0200

    starter

--- 2_awscli/create-ec2.sh
+++ 2_awscli/create-ec2.sh
@@ -26,6 +26,18 @@ EC2_AMI_ID=$(aws ec2 describe-images \
 
 echo "Got AMI Id: ${EC2_AMI_ID}"  # Returns ami-0fc5d935ebf8bc3bc
 
+echo ""
+echo ""
+
+echo "AMI => "
+aws ec2 describe-images \
+  --filters Name=owner-alias,Values=amazon \
+  --filters "Name=name,Values=${EC2_DISTRO}" \
+  --query 'reverse(sort_by(Images, &CreationDate))[0]' | jq .
+
+echo ""
+echo ""
+
 ######################################################################
 # Get the VPC Id
 ######################################################################
@@ -45,29 +57,7 @@ echo ""
 echo ""
 
 echo "VPC => "
-aws ec2 describe-vpcs | jq .
-#{
-#  "Vpcs": [
-#    {
-#      "CidrBlock": "172.31.0.0/16",
-#      "DhcpOptionsId": "dopt-0a01114910d27220e",
-#      "State": "available",
-#      "VpcId": "vpc-0281720e47f44a02f",
-#      "OwnerId": "020887103976",
-#      "InstanceTenancy": "default",
-#      "CidrBlockAssociationSet": [
-#        {
-#          "AssociationId": "vpc-cidr-assoc-05e8b80bd3d82447d",
-#          "CidrBlock": "172.31.0.0/16",
-#          "CidrBlockState": {
-#            "State": "associated"
-#          }
-#        }
-#      ],
-#      "IsDefault": true
-#    }
-#  ]
-#}
+aws ec2 describe-vpcs --filters Name=is-default,Values=true | jq .
 
 echo ""
 echo ""
@@ -80,8 +70,21 @@ echo "Getting Subnet Id..."
 
 ### TODO
 EC2_SUBNET_ZONE=""
+EC2_SUBNET_ID=$(aws ec2 describe-subnets \
+  --filters Name=vpc-id,Values=${EC2_VPC_ID} \
+  --query 'Subnets[0].SubnetId' \
+  --output text)
 
 echo "Got Subnet Id: ${EC2_SUBNET_ID}"
+
+echo ""
+echo ""
+
+echo "SUBNET => "
+#aws ec2 describe-vpcs | jq .
+
+echo ""
+echo ""
 
 ######################################################################
 # Get the Security groups Id
@@ -90,6 +93,14 @@ echo "Got Subnet Id: ${EC2_SUBNET_ID}"
 echo "Getting Security Group Id..."
 
 ### TODO
+EC2_SG_NAME="ops"
+
+echo "Getting Security Group Id..."
+EC2_SG_ID=$(aws ec2 describe-security-groups \
+                --filters "Name=vpc-id,Values=${EC2_VPC_ID}" \
+                --group-names "${EC2_SG_NAME}" \
+                --query "SecurityGroups[0].GroupId" \
+                --output text)
 
 echo "Got Security Group Id: ${EC2_SG_ID}"
 
@@ -100,6 +111,14 @@ echo "Got Security Group Id: ${EC2_SG_ID}"
 echo "Provisioning EC2 instance..."
 
 ### TODO
+#EC2_PROVISION=$(aws ec2 run-instances \
+#  --image-id ${EC2_AMI_ID} \
+#  --instance-type t2.micro \
+#  --key-name awscli \
+#  --security-group-ids ${EC2_SG_ID} \
+#  --subnet-id ${EC2_SUBNET_ID} \
+#  --query 'Instances[0].InstanceId' \
+#  --output text)
 
 echo "Ec2 Instance ready, here are the details:"
 echo ${EC2_PROVISION}
