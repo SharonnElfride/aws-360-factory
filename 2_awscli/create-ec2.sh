f11506bbd614875987506dc5a20f3c3c57325e9e
Sharonn <sharonn.zounon@clarilog.com>
2023-09-20 17:36:04 +0200

    starter

--- 2_awscli/create-ec2.sh
+++ 2_awscli/create-ec2.sh
@@ -6,6 +6,9 @@ set -e
 # Variable definition
 ######################################################################
 
+# Filter on the AMI name
+# We want something like:
+EC2_DISTRO="ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"
 
 ######################################################################
 # Get the AMI_ID
@@ -14,8 +17,14 @@ set -e
 echo "Getting AMI Id..."
 
 # TODO
+# aws ec2 describe-images --filters Name=owner-alias,Values=amazon --filters 'Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*' --query 'reverse(sort_by(Images, &CreationDate))[0].ImageId' --output text
+EC2_AMI_ID=$(aws ec2 describe-images \
+  --filters Name=owner-alias,Values=amazon \
+  --filters "Name=name,Values=${EC2_DISTRO}" \
+  --query 'reverse(sort_by(Images, &CreationDate))[0].ImageId' \
+  --output text)
 
-echo "Got AMI Id: ${EC2_AMI_ID}"
+echo "Got AMI Id: ${EC2_AMI_ID}"  # Returns ami-0fc5d935ebf8bc3bc
 
 ######################################################################
 # Get the VPC Id
@@ -24,8 +33,44 @@ echo "Got AMI Id: ${EC2_AMI_ID}"
 echo "Getting VPC Id..."
 
 ### TODO
+EC2_VPC_ID=$(aws ec2 describe-vpcs \
+  --filters Name=is-default,Values=true \
+  --query 'Vpcs[0].VpcId' \
+  --output text)
+#  --query 'sort_by(Vpcs, &State)[0].VpcId' \
 
-echo "Got VPC Id: ${EC2_VPC_ID}"
+echo "Got VPC Id: ${EC2_VPC_ID}"  # Returns vpc-0281720e47f44a02f
+
+echo ""
+echo ""
+
+echo "VPC => "
+aws ec2 describe-vpcs | jq .
+#{
+#  "Vpcs": [
+#    {
+#      "CidrBlock": "172.31.0.0/16",
+#      "DhcpOptionsId": "dopt-0a01114910d27220e",
+#      "State": "available",
+#      "VpcId": "vpc-0281720e47f44a02f",
+#      "OwnerId": "020887103976",
+#      "InstanceTenancy": "default",
+#      "CidrBlockAssociationSet": [
+#        {
+#          "AssociationId": "vpc-cidr-assoc-05e8b80bd3d82447d",
+#          "CidrBlock": "172.31.0.0/16",
+#          "CidrBlockState": {
+#            "State": "associated"
+#          }
+#        }
+#      ],
+#      "IsDefault": true
+#    }
+#  ]
+#}
+
+echo ""
+echo ""
 
 ######################################################################
 # Get the Subnet Id within the VPC
@@ -34,6 +79,7 @@ echo "Got VPC Id: ${EC2_VPC_ID}"
 echo "Getting Subnet Id..."
 
 ### TODO
+EC2_SUBNET_ZONE=""
 
 echo "Got Subnet Id: ${EC2_SUBNET_ID}"
 
